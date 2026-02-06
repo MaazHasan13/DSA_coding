2147119b6a7816cf4111cb047c64281fba3c8c1b
maaz <maazk2414@gmail.com>
2026-02-06 10:50:56 +0530

    dsa solved in optimze way

---
+++ 3_sum_problem.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+
+int sum_3(int arr[],int n,int target){
+    sort(arr,arr+n);
+
+    for(int i=0;i<n-2;i++){
+        int start = i+1;
+        int end = n-1;
+        while(start<end){
+            int ans = arr[i] + arr[start] + arr[end];
+            if(ans==target){
+                return 1;
+
+            }
+            else if(ans>target)
+            end--;
+            else
+            start++;
+        }
+    }
+    return 0 ;
+
+    }
+int main(){
+    int arr[6] = {1,4,45,6,10,8};
+
+    cout<<sum_3(arr,6,24);
+
+
+}
